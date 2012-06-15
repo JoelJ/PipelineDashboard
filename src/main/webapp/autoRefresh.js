79d11cc32b845779acecf65b1acc6262e5f5a35b
Joel <johnson.joel.86@gmail.com>
2012-06-15 08:34:17 -0600

    Pulling the ajax update function out so it's not a closure anymore.

--- src/main/webapp/autoRefresh.js
+++ src/main/webapp/autoRefresh.js
@@ -3,43 +3,45 @@ Event.observe(window, 'load', function() {
 	intervalID = window.setInterval(update, 10000);
 });
 
+function onUpdateSuccess(transport) {
+	var resultBody = transport.responseText;
+
+	var myDiv = $(document.createElement("div"));
+	myDiv.innerHTML = resultBody;
+
+	var newPipelineDashboard = myDiv.getElementsBySelector('#PipelineDashboard');
+	if(newPipelineDashboard.length > 0) {
+		var oldPipelineDashboard = $('PipelineDashboard');
+		oldPipelineDashboard.innerHTML = newPipelineDashboard[0].innerHTML;
+	}
+
+	if(window.initMagicPopup) {
+		window.initMagicPopup();
+	}
+	if(window.initMain) {
+		window.initMain();
+	}
+}
+
 function update() {
 	var url = document.URL;
 	new Ajax.Request(url, {
 		method: 'get',
 		evalJS: 'false',
-		onSuccess: function(transport) {
-			var resultBody = transport.responseText;
-
-			var myDiv = $(document.createElement("div"));
-			myDiv.innerHTML = resultBody;
-
-			var newPipelineDashboard = myDiv.getElementsBySelector('#PipelineDashboard');
-			if(newPipelineDashboard.length > 0) {
-				var oldPipelineDashboard = $('PipelineDashboard');
-				oldPipelineDashboard.innerHTML = newPipelineDashboard[0].innerHTML;
-			}
-
-			if(window.initMagicPopup) {
-				window.initMagicPopup();
-			}
-			if(window.initMain) {
-				window.initMain();
-			}
-		},
+		onSuccess: onUpdateSuccess,
 		onError: function(transport) {
 			console.log(transport);
 		}
 	});
 }
 
-Event.observe(window, 'focus', function() {
-	window.clearInterval(intervalID);
-	intervalID = window.setInterval(update, 10000);
-	update();
-});
-
-Event.observe(window, 'blur', function() {
-	window.clearInterval(intervalID);
-	intervalID = window.setInterval(update, 20000);
-});
+//Event.observe(window, 'focus', function() {
+//	window.clearInterval(intervalID);
+//	intervalID = window.setInterval(update, 10000);
+//	update();
+//});
+//
+//Event.observe(window, 'blur', function() {
+//	window.clearInterval(intervalID);
+//	intervalID = window.setInterval(update, 20000);
+//});
